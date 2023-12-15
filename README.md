e6512c9ce1742c5f659261b4d688082d054a3d43
jsmile <jsmile@naver.com>
2023-12-15 16:39:18 +0900

    riverpod Notifer Provider starter commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# riverpod_notifier_provider
+
+A new Flutter project.
---
+++ lib/src/app.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'pages/counter/counter_page.dart';
+import 'widgets/custom_button.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NotifierProvider',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NotifierProvider'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        shrinkWrap: true,
+        children: const [
+          CustomButton(
+            title: 'Counter',
+            child: CounterPage(),
+          ),
+        ],
+      ),
+    );
+  }
+}
